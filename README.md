fc8606ab1e0f052d71f2985a043cdbb81f237b22
filmod <fmodigh@kth.se>
2021-04-26 21:18:03 +0200

    Add work planning.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DeepLearning21-Project
+
+### Work distribution
+1. Augmentation and sharpening: Filippa
+2. MixUp: Filippa
+3. Model setup: AAR
+4. Label guessing + Loss function: AAR
---
+++ mixmatch.py
@@ -0,0 +1,15 @@
+"""
+Class MixMatch as a representation of the mixmatch algorithm.
+Every part of the algorithm corresponds to a function in the class.
+"""
+
+
+class MixMatch:
+    def augment(self):
+        print("TODO: Insert your implementation here")
+
+    def guessLabel(self):
+        print("TODO: Insert your implementation here")
+
+    def mixUp(self):
+        print("TODO: Insert your implementation here")
